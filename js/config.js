8c9b97be911aaa512f21b9e5e40808dd73c00474
Nurul-ITBM <oshin.uyha@gmail.com>
2026-08-14 10:30:16 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// js/config.js - Konfigurasi Global Client-Side
+
+// Masukkan URL Deployment Apps Script Anda di sini (Cukup di satu tempat ini saja!)
+const CONFIG = {
+    API_URL: 'https://script.google.com/macros/s/AKfycbxrqI_9eKjrWut8zaWBAoy4mMtyYbgVIOvqsxzKc3PTJU-5VmVJdCj7Mjo8tvc9vJYg/exec'
+};
