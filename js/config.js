2a5b852577de21e105fbd1f6953f8bbd116caa1d
Nurul-ITBM <oshin.uyha@gmail.com>
2026-08-18 14:23:04 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 
 // Masukkan URL Deployment Apps Script Anda di sini (Cukup di satu tempat ini saja!)
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbxrqI_9eKjrWut8zaWBAoy4mMtyYbgVIOvqsxzKc3PTJU-5VmVJdCj7Mjo8tvc9vJYg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbzrrbV8sDbATnGg7iVxue07AbdAjuc42Ee-QLJyNo-KBJlzo0D9Jt5zDpfDqSKblBJSZg/exec'
 };
